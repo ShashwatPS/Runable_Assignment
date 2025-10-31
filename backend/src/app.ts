68e73f4bc47d01a1bbd99ada4689c5dce47978c2
Shashwat Singh <shashwat123student@gmail.com>
2025-11-01 02:51:04 +0530

    Configured cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import pageRoutes from './routes/pagesRoutes.js';
 import blockRotes from './routes/blockRoutes.js'
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/page', pageRoutes);
 app.use('/block', blockRotes);
